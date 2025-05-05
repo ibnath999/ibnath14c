398f7a268a826cb86d47ddf1a1a93d60c0de5dc3
ibnath999 <144888607+ibnath999@users.noreply.github.com>
2025-05-06 00:57:04 +0600

    Update ibnu.c

--- ibnu.c
+++ ibnu.c
@@ -4,28 +4,39 @@ int main() {
     int n, i;
     printf("Enter number of processes: ");     scanf("%d", &n);
 
-    int arrival[n], burst[n], waiting[n], turnaround[n], completion = 0;
+    int arrival[n], burst[n], completion[n], waiting[n], turnaround[n];
 
     printf("Enter arrival time and burst time for each process:\n");
-    for (i = 0; i < n; i++) {         printf("Process %d: ", i + 1);         scanf("%d %d", &arrival[i], &burst[i]);
-    }
-
-    // Sorting processes based on arrival time (Bubble Sort)
-    for (i = 0; i < n - 1; i++) {         for (int j = 0; j < n - i - 1; j++) {             if (arrival[j] > arrival[j + 1]) {                 // Swap arrival time                 int temp = arrival[j];                 arrival[j] = arrival[j + 1];                 arrival[j + 1] = temp;
-
-                // Swap burst time                 temp = burst[j];                 burst[j] = burst[j + 1];                 burst[j + 1] = temp;
+    for ( int i = 0; i < n; i++) { 
+        for( int j=0; j<n-i-1;j++){
+            if(arrival[j]>arrival[j+1]{
+                //swap arrival time
+                int temp=arrival[j];
+                arrival[j]=arrival[j+1];
+                arrival[j+1]=temp;
+               
+    // Swap burst time                
+                temp = burst[j];               
+                burst[j] = burst[j + 1];                
+                burst[j + 1] = temp;
             }
         }
     }
-
-    // Calculating waiting time and turnaround time
-    for (i = 0; i < n; i++) {         if (completion < arrival[i])             completion = arrival[i];
-
-        waiting[i] = completion - arrival[i];         turnaround[i] = waiting[i] + burst[i];         completion += burst[i];
+int time=0;
+    // Calculating waiting time and turnaround time, completion time
+    for (int i = 0; i < n; i++) { 
+        if(time< arrival[i]{
+            time=arrival[i];
+            completion[i]=time+brust[i];
+            turnaround[i]=completion[i]-arrival[i];
+            waiting[i]=turnaround[i]-burst[i];
+            time=completion[i];
     }
 
-    // Display the result     printf("\nPID\tArrival\tBurst\tWaiting\tTurnaround\n");
-    for (i = 0; i < n; i++) {         printf("%d\t%d\t%d\t%d\t%d\n", i + 1, arrival[i], burst[i], waiting[i], turnaround[i]);
+    // output process table    
+    printf("\nPID\tArrival\tBurst\tcompletion\tWaiting\tTurnaround\n");
+    for ( int i = 0; i < n; i++) {        
+        printf("%d\t%d\t%d\t%d\t%d\t%d\n", i + 1, arrival[i], burst[i],  completion[i], waiting[i], turnaround[i]);
     }
 
     return 0;
